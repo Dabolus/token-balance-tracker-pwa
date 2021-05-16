d027ddc96331918b7d4112e44a7faa26d2c8eb2c
Giorgio Garasto <giorgio@garasto.me>
2021-05-16 08:33:04 +0200

    chore: replace react with preact at build time

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,17 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 import VitePluginLinaria from 'vite-plugin-linaria';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [reactRefresh(), VitePluginLinaria()],
+  resolve: {
+    alias: {
+      ...(mode === 'production' && {
+        react: 'preact/compat',
+        'react-dom': 'preact/compat',
+      }),
+    },
+  },
   build: {
     sourcemap: true,
   },
-});
+}));

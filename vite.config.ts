228d33ca19a67e675638f6408f0293326edad28c
Giorgio Garasto <giorgio@garasto.me>
2021-05-16 08:31:30 +0200

    chore: enable source maps when building

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ import VitePluginLinaria from 'vite-plugin-linaria';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), VitePluginLinaria()],
+  build: {
+    sourcemap: true,
+  },
 });

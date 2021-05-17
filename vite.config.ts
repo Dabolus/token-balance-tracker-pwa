27deafe37d5bd923179960c2e1bd170ab505326e
Giorgio Garasto <giorgio@garasto.me>
2021-05-17 12:49:53 +0200

    fix: linaria configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import VitePluginMacros from 'vite-plugin-babel-macros';
 import VitePluginLinaria from 'vite-plugin-linaria';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [reactRefresh(), VitePluginLinaria()],
+  plugins: [reactRefresh(), VitePluginMacros(), VitePluginLinaria()],
   resolve: {
     alias: {
       ...(mode === 'production' && {

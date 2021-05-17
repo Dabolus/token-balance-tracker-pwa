fb27a204a79873752156c6703460fce2cb54bad3
Giorgio Garasto <giorgio@garasto.me>
2021-05-17 20:09:53 +0200

    feat: wire up wizard and dashboard

--- src/containers/Router.tsx
+++ src/containers/Router.tsx
@@ -1,19 +1,40 @@
 import React, { FunctionComponent, lazy, Suspense } from 'react';
-import { Route, Switch } from 'wouter';
+import { Redirect, Route, Switch, useRoute } from 'wouter';
 import CenteredLoader from '../components/CenteredLoader';
+import { useProfiles } from '../providers/ProfilesProvider';
+
+const Dashboard = lazy(() => import('./Dashboard'));
+const Wizard = lazy(() => import('./Wizard'));
 
 const Router: FunctionComponent = () => {
-  return (
+  const [isOnWizardPage] = useRoute('/wizard');
+
+  const { profiles } = useProfiles();
+
+  if (profiles && Object.keys(profiles).length > 0 && isOnWizardPage) {
+    return <Redirect to="/" />;
+  }
+
+  if (profiles && Object.keys(profiles).length < 1 && !isOnWizardPage) {
+    return <Redirect to="/wizard" />;
+  }
+
+  return profiles ? (
     <Suspense fallback={<CenteredLoader />}>
       <Switch>
         <Route path="/">
-          <>Dashboard</>
+          <Dashboard />
+        </Route>
+        <Route path="/wizard">
+          <Wizard />
         </Route>
         <Route path="/settings">
           <>Settings</>
         </Route>
       </Switch>
     </Suspense>
+  ) : (
+    <CenteredLoader />
   );
 };
 

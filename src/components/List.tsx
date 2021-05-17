85dfbd18b7a6db631f05f7cabc6ff21021a32ed6
Giorgio Garasto <giorgio@garasto.me>
2021-05-17 20:08:30 +0200

    feat: implement shared list component

---
+++ src/components/List.tsx
@@ -0,0 +1,19 @@
+import { HTMLAttributes } from 'react';
+import { styled } from '@linaria/react';
+
+export interface ListProps extends HTMLAttributes<HTMLDivElement> {
+  gap?: number | string;
+}
+
+const computeCssGap = (gap: string | number = 16) =>
+  typeof gap === 'string' ? gap : `${gap}px`;
+
+const List = styled.div<ListProps>`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ gap }) => computeCssGap(gap)};
+  margin: ${({ gap }) => `${computeCssGap(gap)} auto`};
+  max-width: 720px;
+`;
+
+export default List;

1f278508e0fda6940c33cbb12f76c076b5bb5efc
Giorgio Garasto <giorgio@garasto.me>
2021-05-17 08:19:52 +0200

    feat: implement loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent } from 'react';
+import { styled } from '@linaria/react';
+
+export interface LoaderProps {
+  size?: number;
+}
+
+const Axes = styled.path`
+  fill: none;
+  stroke: #000;
+  stroke-width: 2;
+`;
+
+const Line = styled.path`
+  fill: none;
+  stroke: #00f;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-dasharray: 66.05183410644531;
+  stroke-dashoffset: 66.05183410644531;
+  animation: 2s ease-in-out infinite dash;
+
+  @keyframes dash {
+    25%,
+    50% {
+      stroke-dashoffset: 0;
+    }
+    90%,
+    100% {
+      stroke-dashoffset: -66.05183410644531;
+    }
+  }
+`;
+
+const Loader: FunctionComponent<LoaderProps> = ({ size = 40 }) => (
+  <svg viewBox="0 0 64 64" width={size} height={size}>
+    <Axes d="M 1,1 1,63 63,63" />
+    <Line d="M 5,40 24,24 40,32 60,20" />
+  </svg>
+);
+
+export default Loader;

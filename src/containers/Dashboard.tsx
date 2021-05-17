244ee34049b8ed7f53c8c583887e7905f6521de3
Giorgio Garasto <giorgio@garasto.me>
2021-05-17 20:09:30 +0200

    feat: implement dashboard component

---
+++ src/containers/Dashboard.tsx
@@ -0,0 +1,37 @@
+import React, { FunctionComponent } from 'react';
+import Avatar from '../components/Avatar';
+import CenteredLoader from '../components/CenteredLoader';
+import List from '../components/List';
+import TopBar from '../components/TopBar';
+import TotalBalance from '../components/TotalBalance';
+import TokensBalances from '../components/TokensBalances';
+import useAddressesBalances from '../hooks/useAddressesBalances';
+import { useProfiles } from '../providers/ProfilesProvider';
+
+const Dashboard: FunctionComponent = () => {
+  const { selectedProfile, profiles } = useProfiles();
+
+  const { data } = useAddressesBalances({
+    variables: {
+      config:
+        profiles && selectedProfile ? profiles[selectedProfile].config : [],
+    },
+    skip: !profiles || !selectedProfile,
+  });
+
+  return profiles && selectedProfile ? (
+    <>
+      <TopBar>
+        <Avatar selectedProfile={selectedProfile} profiles={profiles} />
+      </TopBar>
+      <List>
+        <TotalBalance balances={data} />
+        <TokensBalances balances={data} />
+      </List>
+    </>
+  ) : (
+    <CenteredLoader />
+  );
+};
+
+export default Dashboard;

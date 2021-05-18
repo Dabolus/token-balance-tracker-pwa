a2cc3f85d2cc97efde3ae858d848f728bfcbd9c4
Giorgio Garasto <giorgio@garasto.me>
2021-05-18 08:12:10 +0200

    chore: increase poll interval to 20 seconds

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -17,7 +17,7 @@ const Dashboard: FunctionComponent = () => {
         profiles && selectedProfile ? profiles[selectedProfile].config : [],
     },
     skip: !profiles || !selectedProfile,
-    pollInterval: 5000,
+    pollInterval: 20000,
   });
 
   return profiles && selectedProfile ? (

4332e4601a534eda83fb8a2cb605abe4eaffeb90
Giorgio Garasto <giorgio@garasto.me>
2021-05-18 07:15:58 +0200

    fix: combined data computation issues

--- src/hooks/useAddressesBalances.ts
+++ src/hooks/useAddressesBalances.ts
@@ -9,23 +9,43 @@ export interface AddressBalanceWithNetwork extends AddressBalance {
   network: EthereumNetwork;
 }
 
+const addNetwork = (balances: AddressBalance[], network: EthereumNetwork) =>
+  balances.map(data => ({
+    ...data,
+    network,
+  }));
+
 const computeCombinedData = (
   ethBalances?: AddressBalance[],
   bscBalances?: AddressBalance[],
+  ethBalanceSkipped?: boolean,
+  bscBalanceSkipped?: boolean,
 ) => {
+  // If both queries are configured to be skipped, there's no data to return
+  if (ethBalanceSkipped && bscBalanceSkipped) {
+    return;
+  }
+
+  // If ETH query is skipped and we have the BSC data, we can just return it
+  if (ethBalanceSkipped && bscBalances) {
+    return addNetwork(bscBalances, EthereumNetwork.BSC);
+  }
+
+  // Same but opposite
+  if (bscBalanceSkipped && ethBalances) {
+    return addNetwork(ethBalances, EthereumNetwork.ETHEREUM);
+  }
+
+  // If we reach this point, none of the queries have been skipped, so we return undefined
+  // if we don't have the data of one of the two queries yet.
   if (!ethBalances || !bscBalances) {
     return;
   }
 
+  // Finally, we have all the data, so we can just combine it
   return [
-    ...ethBalances.map(data => ({
-      ...data,
-      network: EthereumNetwork.ETHEREUM,
-    })),
-    ...bscBalances.map(data => ({
-      ...data,
-      network: EthereumNetwork.BSC,
-    })),
+    ...addNetwork(ethBalances, EthereumNetwork.ETHEREUM),
+    ...addNetwork(bscBalances, EthereumNetwork.BSC),
   ];
 };
 
@@ -48,6 +68,11 @@ const useAddressesBalances = ({
     {},
   );
 
+  const shouldSkipEthBalance =
+    skip || (transformedConfig[EthereumNetwork.ETHEREUM] || []).length < 1;
+  const shouldSkipBscBalance =
+    skip || (transformedConfig[EthereumNetwork.BSC] || []).length < 1;
+
   const {
     loading: loadingEthAddressesBalances,
     data: ethAddressesBalances,
@@ -57,8 +82,7 @@ const useAddressesBalances = ({
       network: EthereumNetwork.ETHEREUM,
       addresses: transformedConfig[EthereumNetwork.ETHEREUM] || [],
     },
-    skip:
-      skip || (transformedConfig[EthereumNetwork.ETHEREUM] || []).length < 1,
+    skip: shouldSkipEthBalance,
   });
 
   const {
@@ -70,13 +94,18 @@ const useAddressesBalances = ({
       network: EthereumNetwork.BSC,
       addresses: transformedConfig[EthereumNetwork.BSC] || [],
     },
-    skip: skip || (transformedConfig[EthereumNetwork.BSC] || []).length < 1,
+    skip: shouldSkipBscBalance,
   });
 
   return {
     loading: loadingEthAddressesBalances || loadingBscAddressesBalances,
     error: ethAddressesBalancesError || bscAddressesBalancesError || undefined,
-    data: computeCombinedData(ethAddressesBalances, bscAddressesBalances),
+    data: computeCombinedData(
+      ethAddressesBalances,
+      bscAddressesBalances,
+      shouldSkipEthBalance,
+      shouldSkipBscBalance,
+    ),
   };
 };
 
